863478cac9d9df3db7bd63e927b357b445b2d1e4
AlanRacciatti <alanracciatti1220@gmail.com>
2022-10-15 19:57:05 -0300

    fix: variable not declared error

--- test/03-ChooseANickname.test.ts
+++ test/03-ChooseANickname.test.ts
@@ -5,6 +5,7 @@ import { formatBytes32String } from 'ethers/lib/utils';
 import { ethers } from 'hardhat';
 
 describe('DeployAContract', () => {
+  let deployer: SignerWithAddress;
   let attacker: SignerWithAddress;
   let captureTheEther: Contract;
   let target: Contract;
